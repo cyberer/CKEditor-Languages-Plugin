78fb2171adcda05fd821b89754688896a2f633ef
fredck <f.knabben@cksource.com>
2012-03-20 11:30:07 +0100

    The languages plugin is now on Git.

---
+++ README.md
@@ -0,0 +1,31 @@
+﻿CKEditor Languages Plugin
+=========================
+
+This plugin makes is possible to surround contents with &lt;span lang&gt; elements. This is an important requirement for accessibility purposes.
+
+Installation
+------------
+
+1. Create a folder named "plugins/languages" into your CKEditor installation.
+2. In the CKEditor configuration file (config.js) add the following code:
+
+        config.extraPlugins = 'languages';
+    
+        // The languages you want to support (code:name).
+        config.languages = [ 'fr:French', 'es:Spanish', 'it:Italian' ];
+
+3. Add the "Languages" item to your toolbar.
+
+It's also recommended to append some style to &lt;span lang&gt; elements, so users will be able to notice text with language. For that, you may add something like the following to contents.css:
+
+    span[lang]
+    {
+       font-style: italic;
+    }
+
+License
+-------
+
+Licensed under the terms of any of the following licenses at your choice: [GPL](http://www.gnu.org/licenses/gpl.html), [LGPL](http://www.gnu.org/licenses/lgpl.html) and [MPL](http://www.mozilla.org/MPL/MPL-1.1.html).
+
+See LICENSE.md for more information.
---
+++ plugin.js
@@ -0,0 +1,62 @@
+﻿/*
+Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.plugins.add( 'languages',
+{
+	requires : [ 'styles', 'button' ],
+
+	init : function( editor )
+	{
+		var languages = editor.config.languages,
+			items = {};
+
+		for ( var i = 0 ; i < languages.length ; i++ )
+		{
+			var parts = languages[i].split( ':' );
+
+			items[ 'language_' + parts[ 0 ] ] =
+				{
+					label : parts[ 1 ],
+					group : 'languages',
+					order : i,
+					style : new CKEDITOR.style(
+						{
+							element : 'span',
+							attributes : { 'lang' : parts[ 0 ] },
+						}),
+					onClick : function()
+					{
+						this.style.apply( editor.document );
+					}
+				};
+		}
+
+		editor.addMenuGroup( 'languages' );
+		editor.addMenuItems( items );
+
+		editor.ui.add( 'Languages', CKEDITOR.UI_MENUBUTTON,
+			{
+				label : 'Language',
+				title : 'Language',
+				modes : { wysiwyg:1 },
+				className : 'cke_button_languages',
+				icon : this.path + 'icon.png',
+				onMenu : function()
+				{
+					var activeItems = {};
+
+					for ( var prop in items )
+					{
+						if ( items.hasOwnProperty( prop ) )
+							activeItems[ prop ] = CKEDITOR.TRISTATE_ON;
+					}
+					
+					return  activeItems;
+				}
+			});
+	}
+});
+
+CKEDITOR.config.languages = [ 'fr:French', 'es:Spanish', 'it:Italian' ];
